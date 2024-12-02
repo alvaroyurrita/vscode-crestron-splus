6eb10be7376b44efae76f0e509875ff559bbf97a
Alvaro Yurrita <ayurrita@dgicommunications.com>
2024-12-02 16:24:21 -0500

    Insert Category Finished

--- src/simplPlusCategories.ts
+++ src/simplPlusCategories.ts
@@ -1,9 +1,9 @@
 import * as fs from 'fs';
 import path from 'path';
-import { extensions, QuickPickItem, QuickPickOptions, window } from 'vscode';
-export async function insertCategory(){
+import { extensions, Position, QuickPickItem, QuickPickOptions, window } from 'vscode';
+export async function insertCategory() {
     const extensionPath = extensions.getExtension("sentry07.simpl-plus")?.extensionPath;
-    if (extensionPath === undefined) {return;}
+    if (extensionPath === undefined) { return; }
     const categoriesPath = path.join(extensionPath, "categories.json");
     const categories = JSON.parse(fs.readFileSync(categoriesPath, 'utf8')) as QuickPickItem[];
     const quickPickOptions: QuickPickOptions = {
@@ -11,6 +11,28 @@ export async function insertCategory(){
         placeHolder: "Select category to insert",
         matchOnDescription: true,
     };
-    const selection = await window.showQuickPick<any>(categories, quickPickOptions) as QuickPickItem[];
+    const selection = await window.showQuickPick<any>(categories, quickPickOptions) as QuickPickItem;
+    if (selection) {
+        const activeTextDocument = window.activeTextEditor?.document;
+        if (activeTextDocument === undefined) { return; }
+        const activeEditorContents = activeTextDocument.getText();
+        const regex = /#CATEGORY/i;
+        const match = activeEditorContents.match(regex);
+        let insertionPoint = new Position(0, 0);
+        let insertionText = `\"${selection.label}\"`;
+        if (match !== null) {
+            insertionPoint = activeTextDocument.positionAt(match.index!);
+        }
+        if (selection.label === "46") {
+            const customCategory = await window.showInputBox({ prompt: "Enter custom category name" });
+            if (customCategory === undefined) {
+                return;
+            }
+            insertionText = `${insertionText} \"${customCategory}\"`;
+        }
+        await window.activeTextEditor?.edit((editBuilder) => {
+            editBuilder.insert(insertionPoint, `#CATEGORY ${insertionText} \/\/${selection.description}\n`);
+        });
+    }
 
 }
